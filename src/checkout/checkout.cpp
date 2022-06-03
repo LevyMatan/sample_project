2d4766634ba41384c0880d5885afb09d238b0496
Matan Levy <levymatanlevy@gmail.com>
2022-06-03 13:08:24 +0300

    Remove trailing white spaces

--- src/checkout/checkout.cpp
+++ src/checkout/checkout.cpp
@@ -1,12 +1,12 @@
-#include "checkout.hpp" 
+#include "checkout.hpp"
 
 Checkout::Checkout():total(0)
 {
-    
+
 }
 Checkout::~Checkout()
 {
-    
+
 }
 
 void Checkout::addItemPrice(std::string item_name, int item_price)
@@ -20,19 +20,19 @@ void Checkout::addItem(std::string item_name)
     {
         throw std::invalid_argument("Trying to add item which is not on the items price list!");
     }
-    
+
     items[item_name]++;
 }
 
 void Checkout::addDiscount(std::string item_name, int number_of_items, int discount_price)
 {
-    Discount discount; 
+    Discount discount;
     discount.discount_price = discount_price;
     discount.num_of_items = number_of_items;
 
-    
+
     discounts[item_name] = discount;
-    
+
 }
 
 int Checkout::calculateTotal(void)
@@ -47,17 +47,17 @@ int Checkout::calculateTotal(void)
 }
 
 /**
- * @brief 
- * 
- * @param[in] item_itr 
- * @return int 
+ * @brief
+ *
+ * @param[in] item_itr
+ * @return int
  */
 int Checkout::calculateItem(std::map<std::string, int>::iterator item_itr)
 {
     int item_price;
     std::string item_name = item_itr->first;
     int item_count = item_itr->second;
-    
+
     std::map<std::string, Discount>::iterator discount_itr;
     discount_itr = discounts.find(item_name);
     if(discount_itr != discounts.end())
@@ -69,6 +69,6 @@ int Checkout::calculateItem(std::map<std::string, int>::iterator item_itr)
     {
         item_price = item_count * items_prices_list[item_name];
     }
-    
+
     return item_price;
 }
